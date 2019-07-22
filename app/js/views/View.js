1d970f68185778e3325c4414c333b8f73516474e
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2019-07-22 09:39:56 -0300

    Implementando carregador de módulos

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,11 +1,22 @@
-class View {
-    constructor(seletor) {
-        this._elemento = $(seletor);
-    }
-    update(model) {
-        this._elemento.html(this.template(model));
-    }
-    template(model) {
-        throw new Error('Você deve implementar o método template');
-    }
-}
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var View;
+    return {
+        setters: [],
+        execute: function () {
+            View = class View {
+                constructor(seletor) {
+                    this._elemento = $(seletor);
+                }
+                update(model) {
+                    this._elemento.html(this.template(model));
+                }
+                template(model) {
+                    throw new Error('Você deve implementar o método template');
+                }
+            };
+            exports_1("View", View);
+        }
+    };
+});

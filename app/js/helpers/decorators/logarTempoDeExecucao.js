506602ac8ffc9512d62a74cb88cf5077761e7e81
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2019-07-23 10:11:41 -0300

    Medindo o tempo de execução de métodos

--- app/js/helpers/decorators/logarTempoDeExecucao.js
+++ app/js/helpers/decorators/logarTempoDeExecucao.js
@@ -1,11 +1,23 @@
 System.register([], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    function logarTempoDeExecucao() {
+    function logarTempoDeExecucao(emSegundos = false) {
         return function (target, propertyKey, descriptor) {
             const metodoOriginal = descriptor.value;
             descriptor.value = function (...args) {
+                let unidade = 'ms';
+                let divisor = 1;
+                if (emSegundos) {
+                    unidade = 's';
+                    divisor = 1000;
+                }
+                console.log('-----------------');
+                console.log(`parametros passados para o método ${propertyKey}: ${JSON.stringify(args)}`);
+                const t1 = performance.now();
                 const retorno = metodoOriginal.apply(this, args);
+                const t2 = performance.now();
+                console.log(`O retorno do méto ${propertyKey} é ${JSON.stringify(retorno)}`);
+                console.log(`O método ${propertyKey} demorou ${(t2 - t1) / divisor} ${unidade}`);
                 return retorno;
             };
             return descriptor;

b1281d2cf1d9416f2bff4559c119ad1e6f18ee7e
Murillo Bilches Navarro <mnavarro@HYPERATIVA.NET>
2019-08-08 11:46:54 -0300

    Adicionando mensagem de alto nível.

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -74,7 +74,10 @@ export class NegociacaoController {
                         this._negociacoes.adiciona(negociacao))
                 
                 this._negociacoesView.update(this._negociacoes);
-            });     
+            }) 
+            .catch(err => {
+                this._mensagemView.update(err.message);
+            });
     }
 }
 
